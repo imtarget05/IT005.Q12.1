f76313d72167bfb15b47f37a5e2b0c10eb21680c
hihiuwin <ledinhhuythibmt@gmail.com>
2026-03-24 14:32:38 +0700

    task 2

--- Lab02_Modern_Cryptography/src/task2_2.py
+++ Lab02_Modern_Cryptography/src/task2_2.py
@@ -1,1 +1,14 @@
-asdaada
+from Crypto.Cipher import AES
+
+key = b'1234567890123456'
+plaintext = b"UIT_LAB_UIT_LAB_UIT_LAB_UIT_LAB_" 
+
+cipher_ecb = AES.new(key, AES.MODE_ECB)
+ct_ecb = cipher_ecb.encrypt(plaintext)
+
+iv = b'\x00' * 16
+cipher_cbc = AES.new(key, AES.MODE_CBC, iv=iv)
+ct_cbc = cipher_cbc.encrypt(plaintext)
+
+print(f"ECB Hex: {ct_ecb.hex()}")
+print(f"CBC Hex: {ct_cbc.hex()}\n")
